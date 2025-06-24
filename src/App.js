35be185fb9e63e4081e33dcee31171223b7a486e
Gaelle Boucher <gaelleboucher.dev@gmail.com>
2025-06-24 22:11:31 +0200

    Premier affichage OK : logo, nav, titre. Projet lancé !

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './Logo GB WebAssist.webp';
+import React from 'react';
 import './App.css';
+import logo from './Logo GB WebAssist.webp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app">
+      <header className="navbar">
+        <a href="#home">
+          <img src={logo} alt="Logo GB WebAssist" height="60" />
         </a>
+        <nav>
+          <a href="#services">Prestations</a>
+          <a href="#contact">Contact</a>
+        </nav>
       </header>
+
+      <main>
+        <section id="home">
+          <h1>Bienvenue sur mon site</h1>
+          <p></p>
+        </section>
+      </main>
     </div>
   );
 }
